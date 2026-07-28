9be83ac06366a6871f17f9ad6cbcfdd54da99150
osaka <osaka242128@gmail.com>
2026-07-29 04:27:16 +0530

    feat : add ReturnNode for function statments return

--- core/nodes.py
+++ core/nodes.py
@@ -125,6 +125,14 @@ class CallNode:
                self.pos_end = self.node_to_call.pos_end
 
 
+class ReturnNode:
+     def __init__(self,node_to_return,pos_start,pos_end):
+          self.node_to_return = node_to_return
+
+          self.pos_start = pos_start
+          self.pos_end = pos_end
+
+
 class StringNode:
      def __init__(self,tok):
           self.tok = tok
