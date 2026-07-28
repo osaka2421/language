ade2ae1340ac2b418f4d5e58449c521d81c1b0ca
osaka <osaka242128@gmail.com>
2026-07-29 04:35:29 +0530

    fix : improve runtime traceback generation

--- errors/Error.py
+++ errors/Error.py
@@ -39,6 +39,8 @@ class RTError(Error):
 
 
           while ctx:
+                print(pos)
+                print(ctx)
                 result = f' File {pos.fn} , line {str(pos.ln + 1)} , in {ctx.display_name}\n' + result
                 pos = ctx.parent_entry_pos
                 ctx = ctx.parent
