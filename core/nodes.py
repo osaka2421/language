201e9bbc137dc2b9042d937ead2ae8283f8c234e
osaka <osaka242128@gmail.com>
2026-08-11 05:15:28 +0530

    feat : add IndexNodes

--- core/nodes.py
+++ core/nodes.py
@@ -146,6 +146,16 @@ class ListExprNode:
           self.pos_start = pos_start
           self.pos_end = pos_end
 
+
+class IndexNode :
+     def __init__(self, list_node, index_node):
+          self.list_node = list_node
+          self.index_node = index_node
+
+          self.pos_start = list_node.pos_start 
+          self.pos_end = index_node.pos_end
+          
+
      
 class UnaryOpNode:
      def __init__(self,op_tok,node):
