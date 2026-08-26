80392fb0ee8a0a75fd8157e473b72acc8119c073
osaka <osaka242128@gmail.com>
2026-08-26 19:37:06 +0530

    feat: add LenNode

--- core/nodes.py
+++ core/nodes.py
@@ -162,6 +162,15 @@ class IndexAssignNode:
 
           self.pos_start = index_node.pos_start
           self.pos_end = value_node.pos_end 
+
+
+class LenNode:
+     def __init__(self,value_node):
+          self.value_node = value_node
+
+          self.pos_start = value_node.pos_start
+          self.pos_end = value_node.pos_end
+          
           
           
 class UnaryOpNode:
